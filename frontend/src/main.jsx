99ff5aa93e25ec9e3effedcc3540184f8cc6b879
ledemkam <ledemkam@gmail.com>
2024-11-06 16:49:38 +0100

    tansquery implmnt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryProvider } from './lib/react-query/QueryProviders.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <QueryProvider>
+        <App />
+    </QueryProvider>
+  </BrowserRouter>
 )
